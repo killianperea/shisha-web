cbe4e6700431f7c4b82df18bd6de004d526bbad9
Killian <killianperea@gmail.com>
2021-02-27 13:46:50 +0100

    Add models wip

---
+++ packages/shisha-web-sanity/schemas/documents/index.js
@@ -0,0 +1,3 @@
+export { default as Brand } from './Brand';
+export { default as Flavor } from './Flavor';
+export { default as Tabacco } from './Tabacco';
--- packages/shisha-web-sanity/schemas/schema.js
+++ packages/shisha-web-sanity/schemas/schema.js
@@ -3,6 +3,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import { Brand, Flavor, Tabacco } from './documents';
+import localeString from './objects/LocaleString';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +13,9 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    localeString,
+    Brand,
+    Tabacco,
+    Flavor
   ]),
 })
