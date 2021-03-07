bb821ed09979d0f7918a7b1196ce9cea11f04cc4
Killian <killianperea@gmail.com>
2021-03-07 19:52:42 +0100

    remove unused css file

--- packages/shisha-web-nextjs/pages/_app.tsx
+++ packages/shisha-web-nextjs/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import '../styles/globals.css';
 import CustomTheme from '../theme/CustomTheme';
 
 const App = ({ Component, pageProps }): JSX.Element => {
