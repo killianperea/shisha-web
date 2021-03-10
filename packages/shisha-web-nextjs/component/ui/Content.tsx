c053e934e7397a99d848a4430154fdb62af6061e
Killian <killianperea@gmail.com>
2021-03-10 22:53:06 +0100

    remove useBreakpoint

--- packages/shisha-web-nextjs/component/ui/Content.tsx
+++ packages/shisha-web-nextjs/component/ui/Content.tsx
@@ -1,17 +1,14 @@
-import { Box, HStack, useBreakpointValue } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 
-const Content = ({ children, withFooter = false, ...props}): JSX.Element => {
-
-    const hideAside = useBreakpointValue({ base: true, lg: false });
-    const asideMinWidth = useBreakpointValue({ base: '150px', xl: '200px' });
+const Content = ({ children, withFooter = false, ...props }): JSX.Element => {
 
     const footerPaddingBottom = withFooter ? [4, 4, "9rem", "9rem"] : 4;
 
     return (
         <HStack align="start" isInline={true} spacing="24px" pb={footerPaddingBottom} {...props}>
-            <Box minWidth={asideMinWidth} as="aside" hidden={hideAside}></Box>
-            <Box flex="1" marginRight={["24px", "24px", "24px", "0"]}>{children}</Box>
-            <Box minWidth={asideMinWidth} as="aside" hidden={hideAside}></Box>
+            <Box minWidth={["0", "0", "150px", "200px"]} as="aside" display={["none", "none", "flex", "flex"]}></Box>
+            <Box as="main" flex="1" marginRight={["24px", "24px", "24px", "0"]}>{children}</Box>
+            <Box minWidth={["0", "0", "150px", "200px"]} as="aside" display={["none", "none", "flex", "flex"]}></Box>
         </HStack>
     )
 }
--- packages/shisha-web-nextjs/component/ui/Layout.tsx
+++ packages/shisha-web-nextjs/component/ui/Layout.tsx
@@ -6,13 +6,10 @@ import Header from "./Header/Header";
 
 
 const Layout = ({ children, withHeader = false, withFooter = false }): JSX.Element => {
-
-    const fontSize = useBreakpointValue({base: "12px", md: "14px"})
-
     return (
         <Container>
             { withHeader ? <Header position={['sticky', 'sticky', 'static', 'static']} top="0"/> : null }
-            <Content fontSize={fontSize} withFooter={withFooter}>{children}</Content>
+            <Content fontSize={["12px", "12px","14px","14px"]} withFooter={withFooter}>{children}</Content>
             { withFooter ? <Footer /> : null }
         </Container>
     );
