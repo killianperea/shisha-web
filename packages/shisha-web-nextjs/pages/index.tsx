d62dc8a07c5321fa470d8ebf7bfd2d08df6b1844
Killian <killianperea@gmail.com>
2021-03-15 00:47:22 +0100

    add cookie to /

--- packages/shisha-web-nextjs/pages/index.tsx
+++ packages/shisha-web-nextjs/pages/index.tsx
@@ -1,6 +1,21 @@
+import { GetStaticProps } from "next";
 import RedirectHome from "../component/RedirectHome";
+import { SanityApi } from "../lib/sanityApi";
+import { Utils } from "../lib/utils";
 
 
 const RootPage = (): JSX.Element => <RedirectHome />;
+
+export const getStaticProps: GetStaticProps = async () => {
+
+    const cookies = await SanityApi.getCookiesConsient();
+
+    return {
+        props: {
+            cookies: Utils.serialize(cookies)
+        },
+    };
+};
+
 
 export default RootPage;
